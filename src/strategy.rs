6f3142e2ba262696669b02615818e2f155bb6e88
Max Inden <mail@max-inden.de>
2020-03-19 21:57:54 +0100

    src/strategy: Introduce NoEliminationStrategy

--- src/strategy.rs
+++ src/strategy.rs
@@ -28,3 +28,33 @@ impl StackPopStrategy for DefaultStrategy {
         true
     }
 }
+
+// Strategy to have Stack use the Treiber Stack only and not elude to the
+// elimination array on contention.
+pub struct NoEliminationStrategy {}
+
+impl NoEliminationStrategy {
+    fn new() -> Self {
+        NoEliminationStrategy {}
+    }
+}
+
+impl StackPushStrategy for NoEliminationStrategy {
+    fn new() -> Self {
+        NoEliminationStrategy::new()
+    }
+
+    fn use_elimination_array(&mut self) -> bool {
+        false
+    }
+}
+
+impl StackPopStrategy for NoEliminationStrategy {
+    fn new() -> Self {
+        NoEliminationStrategy::new()
+    }
+
+    fn use_elimination_array(&mut self) -> bool {
+        false
+    }
+}
